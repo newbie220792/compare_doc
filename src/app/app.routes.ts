6eabb02aaab7f99e11b0950092522ba37802932c
Anthony.Doan <anthony.doan@code88.asia>
2026-02-09 09:17:33 +0700

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {App} from './app';
+import {NotFound} from '../components/commons/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'Home',
+    path: '/',
+    component: App,
+    // canActivate: [authGuardFn]
+  },
+  {
+    title: 'Not found',
+    path: '/222',
+    component: NotFound
+  },
+  {
+    title: 'Not found',
+    path: '',
+    pathMatch: 'full',
+    component: NotFound
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,39 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('sso-app');
+  protected readonly title = signal('sso-app', {debugName: 'sso-app'});
+  vite: string = 'vite111';
+
+  constructor() {
+    console.log('constructor')
+  }
+
+  ngOnInit() {
+    console.log('init')
+  }
+
+  ngAfterViewInit() {
+    console.log('afterViewInit')
+  }
+
+  ngOnDestroy() {
+    console.log('destroy')
+  }
+
+  onSelect(event: any) {
+    console.log('onSelect', event)
+  }
+
+  fetchEvent(event: any) {
+    console.log('fetchEvent', event)
+    return new Array(10).fill(0).map((v, i) => i).join(',');
+  }
 }
